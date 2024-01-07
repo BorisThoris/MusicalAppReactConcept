9ce627b42ecd55d1ae29030311d55fb85b378569
Boris <modaxxx009@yahoo.com>
2024-01-08 00:06:50 +0200

    Refactor GroupUtility.js with lodash

--- fmodreact/src/hooks/useOverlapCalculator/GroupUtility.js
+++ fmodreact/src/hooks/useOverlapCalculator/GroupUtility.js
@@ -1,39 +1,42 @@
 /* eslint-disable no-param-reassign */
+import _ from 'lodash';
+import differenceWith from 'lodash/differenceWith';
+import isEqual from 'lodash/isEqual';
+import pullAt from 'lodash/pullAt';
+import some from 'lodash/some';
+import unionWith from 'lodash/unionWith';
+import uniqBy from 'lodash/uniqBy';
 import { createGroupFromEvent } from './EventUtility';
 
 export const filterOutOverlappedGroups = (groups, overlapped) =>
-    groups.filter((group) => !overlapped.includes(group));
+    differenceWith(groups, overlapped, isEqual);
 
 export const mergeGroupWithOverlaps = ({ group, overlapGroup }) => {
     group.startTime = Math.min(group.startTime, overlapGroup.startTime);
     group.endTime = Math.max(group.endTime, overlapGroup.endTime);
-    overlapGroup.events?.forEach((event) => {
-        const containEvent = group.events.some((e) => e.id === event.id);
-        if (!containEvent) group.events.push(event);
-    });
-
-    group.locked = group.locked || overlapGroup.locked || false;
+    group.events = unionWith(
+        group.events,
+        overlapGroup.events,
+        (a, b) => a.id === b.id
+    );
+    group.locked = group.locked || overlapGroup.locked;
 };
 
 export const handleNonOverlappingEvent = (event, interval, groups, tree) => {
     const newGroup = createGroupFromEvent(event);
     tree.insert(interval, newGroup);
     tree.remove([event.startTime, event.endTime], event);
-    if (!groups.some((group) => group.id === event.id)) {
+    if (!some(groups, { id: event.id })) {
         groups.push(newGroup);
     }
 };
 
 export const combineOverlappingGroups = (groups, tree) => {
-    console.log('');
-    console.log('START');
-    console.log(groups);
+    let changed = true;
 
     const doGroupsOverlap = (group1, group2) =>
         group1.endTime >= group2.startTime &&
         group2.endTime >= group1.startTime;
-
-    let changed = true;
 
     while (changed) {
         changed = false;
@@ -49,50 +52,34 @@ export const combineOverlappingGroups = (groups, tree) => {
                         [groups[j].startTime, groups[j].endTime],
                         groups[j]
                     );
-
-                    groups.splice(j, 1);
+                    pullAt(groups, j);
 
                     tree.remove(
                         [groups[i].startTime, groups[i].endTime],
                         groups[i]
                     );
-
                     tree.insert(
                         [groups[i].startTime, groups[i].endTime],
                         groups[i]
                     );
 
                     changed = true;
-
-                    // Break inner loop to restart since groups have changed
                     break;
                 }
             }
-
-            if (changed) {
-                break; // Break outer loop to restart since groups have changed
-            }
+            if (changed) break;
         }
     }
-
-    console.log(groups);
-    console.log('End');
-    console.log('');
 
     return groups;
 };
 
 export const mergeOverlappingEvents = (event, overlaps, groups, tree) => {
     const mergedGroup = createGroupFromEvent(event);
-
-    overlaps.forEach((overlapGroup) => {
-        mergeGroupWithOverlaps({
-            group: mergedGroup,
-            overlapGroup,
-        });
-    });
-
-    mergedGroup.events = Array.from(new Set(mergedGroup.events));
+    overlaps.forEach((overlapGroup) =>
+        mergeGroupWithOverlaps({ group: mergedGroup, overlapGroup })
+    );
+    mergedGroup.events = uniqBy(mergedGroup.events, 'id');
 
     if (!mergedGroup.endTime && !mergedGroup.startTime) {
         mergedGroup.startTime = event.startTime;
@@ -101,14 +88,9 @@ export const mergeOverlappingEvents = (event, overlaps, groups, tree) => {
 
     tree.insert([mergedGroup.startTime, mergedGroup.endTime], mergedGroup);
 
-    // const newGroups = combineOverlappingGroups(tree.values, tree);
-
-    // groups = filterOutOverlappedGroups(groups, tree.values);
-    if (!groups.some((group) => group.id === mergedGroup.id)) {
+    if (!some(groups, { id: mergedGroup.id })) {
         groups.push(mergedGroup);
     }
 
     groups = combineOverlappingGroups(groups, tree);
-
-    console.log(groups);
 };
