df8f4c51777fb18e6cca301a30623c006e3d4ae2
Boris <modaxxx009@yahoo.com>
2024-02-15 00:43:12 +0200

    Further OverlapGroup drag movement effect updates

--- fmodreact/src/components/Editor/components/OverlapGroupElement/OverlapGroupElement.js
+++ fmodreact/src/components/Editor/components/OverlapGroupElement/OverlapGroupElement.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-unsafe-optional-chaining */
-import { isEqual } from 'lodash';
+import get from 'lodash/get';
+import isEqual from 'lodash/isEqual';
 import PropTypes from 'prop-types';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { Group, Rect, Text } from 'react-konva';
 import pixelToSecondRatio from '../../../../globalConstants/pixelToSeconds';
 import { useInstrumentRecordingsOperations } from '../../../../hooks/useInstrumentRecordingsOperations';
@@ -16,8 +16,8 @@ const TEXT_OFFSET_Y = 20;
 const LOCK_OFFSET_Y = -10;
 const TEXT_FONT_SIZE = 18;
 
-const OverlapGroupElement = React.memo(
-    ({
+const OverlapGroupElement = React.memo((props) => {
+    const {
         canvasOffsetY,
         focusedEvent,
         groupData,
@@ -28,129 +28,110 @@ const OverlapGroupElement = React.memo(
         timelineHeight,
         timelineY,
         updateStartTime
-    }) => {
-        const groupElmRef = useRef();
-        const [isDragged, setIsDragged] = useState(false);
-        const { lockOverlapGroupById, updateOverlapGroupTimes } = useInstrumentRecordingsOperations();
+    } = props;
 
-        const { endTime, events, id, instrumentName, locked, startTime } = groupData;
+    const { endTime, events, id, instrumentName, locked, startTime } = groupData;
 
-        const startingPositionInTimeline = startTime * pixelToSecondRatio;
-        const groupWidth = (endTime - startTime) * pixelToSecondRatio;
+    const groupElmRef = useRef();
+    const [isDragged, setIsDragged] = useState(false);
+    const { lockOverlapGroupById, updateOverlapGroupTimes } = useInstrumentRecordingsOperations();
 
-        const handleDragEnd = useCallback(
-            (e) => {
-                setIsDragged(false);
+    const startingPositionInTimeline = startTime * pixelToSecondRatio;
+    const groupWidth = (endTime - startTime) * pixelToSecondRatio;
 
-                const newGroupStart = e.target.x() / pixelToSecondRatio;
-                updateOverlapGroupTimes({
-                    groupId: id,
-                    newStartTime: newGroupStart
-                });
-            },
-            [id, updateOverlapGroupTimes]
-        );
+    const handleDragEnd = useCallback(
+        (e) => {
+            setIsDragged(false);
+            updateOverlapGroupTimes({ groupId: id, newStartTime: e.target.x() / pixelToSecondRatio });
+        },
+        [id, updateOverlapGroupTimes]
+    );
 
-        const handleClickOverlapGroup = useCallback(() => {
-            const fallbackX = 0;
-            const groupX = groupElmRef.current?.parent?.attrs?.x || fallbackX;
-            // eslint-disable-next-line no-unsafe-optional-chaining
-            const groupY = timelineY + canvasOffsetY + groupElmRef.current?.attrs?.height;
-            openPanel({ index, instrumentName, x: groupX, y: groupY });
-        }, [canvasOffsetY, index, instrumentName, openPanel, timelineY]);
+    const handleClickOverlapGroup = useCallback(() => {
+        const groupX = get(groupElmRef, '.current?.parent?.attrs?.x') || 0;
+        const groupY = timelineY + canvasOffsetY + get(groupElmRef, '.current.attrs.height');
+        openPanel({ index, instrumentName, x: groupX, y: groupY });
+    }, [canvasOffsetY, index, instrumentName, openPanel, timelineY]);
 
-        const dragBoundFunc = useCallback((pos) => ({ x: pos.x, y: timelineY }), [timelineY]);
+    const dragBoundFunc = useCallback((pos) => ({ x: pos.x, y: timelineY }), [timelineY]);
 
-        const onLockOverlapGroup = useCallback(() => {
-            lockOverlapGroupById({ groupId: id });
-        }, [id, lockOverlapGroupById]);
+    const onLockOverlapGroup = useCallback(() => {
+        lockOverlapGroupById({ groupId: id });
+    }, [id, lockOverlapGroupById]);
 
-        const onDragStart = useCallback(() => {
-            setIsDragged(true);
-        }, []);
+    const onDragStart = useCallback(() => setIsDragged(true), []);
 
-        return (
-            <Group onClick={handleClickOverlapGroup}>
-                <Group
-                    key={index}
-                    x={startingPositionInTimeline}
-                    draggable={!locked}
-                    onDragEnd={handleDragEnd}
-                    dragBoundFunc={dragBoundFunc}
-                    // eslint-disable-next-line react-perf/jsx-no-new-function-as-prop
-                    onDragStart={onDragStart}
+    const renderEvents = () =>
+        events.map((event, eventIndex) => (
+            <SoundEventElement
+                key={event.id}
+                // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+                parent={groupData}
+                index={eventIndex}
+                isOverlapping={events.length > 1}
+                recording={event}
+                timelineHeight={timelineHeight}
+                timelineY={timelineY}
+                updateStartTime={updateStartTime}
+                isFocused={event.id === focusedEvent}
+                setFocusedEvent={setFocusedEvent}
+                canvasOffsetY={canvasOffsetY}
+            />
+        ));
+
+    return (
+        <Group onClick={handleClickOverlapGroup}>
+            <Group
+                key={index}
+                x={startingPositionInTimeline}
+                draggable={!locked}
+                onDragEnd={handleDragEnd}
+                dragBoundFunc={dragBoundFunc}
+                onDragStart={onDragStart}
+                width={groupWidth}
+            >
+                <Rect
+                    ref={groupElmRef}
                     width={groupWidth}
-                >
-                    <Rect
-                        ref={groupElmRef}
-                        width={groupWidth}
-                        height={timelineHeight * 0.9}
-                        fill={isTargeted ? 'red' : GROUP_COLOR}
-                        opacity={GROUP_OPACITY}
-                        strokeWidth={GROUP_STROKE_WIDTH}
-                        stroke={GROUP_COLOR}
-                    />
-                    <Text
-                        x={TEXT_OFFSET_X}
-                        y={TEXT_OFFSET_Y}
-                        text={GROUP_TEXT}
-                        fontSize={TEXT_FONT_SIZE}
-                        fill="white"
-                    />
+                    height={timelineHeight * 0.9}
+                    fill={isTargeted ? 'red' : GROUP_COLOR}
+                    opacity={GROUP_OPACITY}
+                    strokeWidth={GROUP_STROKE_WIDTH}
+                    stroke={GROUP_COLOR}
+                />
+                <Text x={TEXT_OFFSET_X} y={TEXT_OFFSET_Y} text={GROUP_TEXT} fontSize={TEXT_FONT_SIZE} fill="white" />
+
+                {isDragged && (
+                    <>
+                        <Group x={-startingPositionInTimeline}>{renderEvents()}</Group>
+                        <Text
+                            onClick={onLockOverlapGroup}
+                            x={-10}
+                            y={LOCK_OFFSET_Y}
+                            text={locked ? '🔒' : '✔️'}
+                            fontSize={TEXT_FONT_SIZE}
+                            fill="white"
+                        />
+                    </>
+                )}
+            </Group>
+
+            {!isDragged && (
+                <>
+                    <Group>{renderEvents()}</Group>
                     <Text
                         onClick={onLockOverlapGroup}
-                        x={-10}
+                        x={startingPositionInTimeline - 10}
                         y={LOCK_OFFSET_Y}
                         text={locked ? '🔒' : '✔️'}
                         fontSize={TEXT_FONT_SIZE}
                         fill="white"
                     />
-
-                    {isDragged && (
-                        <Group x={-startingPositionInTimeline}>
-                            {events.map((event, eventIndex) => (
-                                <SoundEventElement
-                                    parent={groupData}
-                                    key={event.id}
-                                    index={eventIndex}
-                                    isOverlapping={events.length > 1}
-                                    recording={event}
-                                    timelineHeight={timelineHeight}
-                                    timelineY={timelineY}
-                                    updateStartTime={updateStartTime}
-                                    isFocused={event.id === focusedEvent}
-                                    setFocusedEvent={setFocusedEvent}
-                                    canvasOffsetY={canvasOffsetY}
-                                />
-                            ))}
-                        </Group>
-                    )}
-                </Group>
-
-                {!isDragged && (
-                    <Group>
-                        {events.map((event, eventIndex) => (
-                            <SoundEventElement
-                                parent={groupData}
-                                key={event.id}
-                                index={eventIndex}
-                                isOverlapping={events.length > 1}
-                                recording={event}
-                                timelineHeight={timelineHeight}
-                                timelineY={timelineY}
-                                updateStartTime={updateStartTime}
-                                isFocused={event.id === focusedEvent}
-                                setFocusedEvent={setFocusedEvent}
-                                canvasOffsetY={canvasOffsetY}
-                            />
-                        ))}
-                    </Group>
-                )}
-            </Group>
-        );
-    },
-    isEqual
-);
+                </>
+            )}
+        </Group>
+    );
+}, isEqual);
 
 OverlapGroupElement.propTypes = {
     canvasOffsetY: PropTypes.number.isRequired,
@@ -159,10 +140,7 @@ OverlapGroupElement.propTypes = {
         endTime: PropTypes.number.isRequired,
         events: PropTypes.arrayOf(
             PropTypes.shape({
-                eventInstance: PropTypes.object.isRequired,
-                eventLength: PropTypes.number.isRequired,
                 id: PropTypes.string.isRequired,
-                instrumentName: PropTypes.string.isRequired,
                 startTime: PropTypes.number.isRequired
             })
         ).isRequired,
